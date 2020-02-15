7a2852f69e4ca587288f42e93bbb3d2f81ece330
Abdul wahid kolleri <45917109+Abdulkolleri@users.noreply.github.com>
2020-02-15 16:30:14 -0500

    feedback page ceated

---
+++ app/src/main/java/com/kulartist/tekhub/Feedback.java
@@ -0,0 +1,16 @@
+package com.kulartist.tekhub;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.kulartist.tekhubandroid.R;
+
+public class Feedback extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_feedback);
+    }
+}
