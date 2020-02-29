7b7ae17ef699af2428180a2b140c755d9eeeb84f
Gilpix <kuldeepsingh5005@gmail.com>
2020-02-29 07:45:51 -0500

    DatabaseObjects class created to hold JSON objects/array of database

---
+++ app/src/main/java/com/kulartist/tekhub/DatabaseObjects.java
@@ -0,0 +1,16 @@
+package com.kulartist.tekhub;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+public class DatabaseObjects {
+
+
+    static JSONObject studentProfile=new JSONObject();
+
+    static JSONArray itemList=new JSONArray();
+
+    static JSONArray orderList=new JSONArray();
+
+    static JSONArray waitingList=new JSONArray();
+}
