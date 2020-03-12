9938b374acfd0ea105d7769239c7beb887bc7c0e
Gilpix <kuldeepsingh5005@gmail.com>
2020-03-12 12:02:28 -0400

    shared preference added for storing the current user state

--- app/src/main/java/com/kulartist/tekhubandroid/SplashScreen.java
+++ app/src/main/java/com/kulartist/tekhubandroid/SplashScreen.java
@@ -1,12 +1,18 @@
 package com.kulartist.tekhubandroid;
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Handler;
 import android.os.Bundle;
+
+import com.kulartist.tekhub.ItemList;
+
 import androidx.appcompat.app.AppCompatActivity;
 
 public class SplashScreen extends AppCompatActivity {
     Handler handler;
+    public static String currentIP="192.168.0.102";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,9 +23,25 @@ public class SplashScreen extends AppCompatActivity {
         handler.postDelayed(new Runnable() {
             @Override
             public void run() {
-                Intent intent=new Intent(SplashScreen.this,LoginActivity.class);
-                startActivity(intent);
-                finish();
+
+                SharedPreferences sp = getSharedPreferences("saveUser" ,Context.MODE_PRIVATE);
+                String highScore = sp.getString("userSavedId", "");
+                LoginActivity.currentUser=highScore;
+
+                System.out.println(LoginActivity.currentUser+"@@@@@@@@@@@@@########"+highScore);
+
+
+                if(highScore.equals("")) {
+                    Intent intent = new Intent(SplashScreen.this, LoginActivity.class);
+                    startActivity(intent);
+                    finish();
+                }
+                else
+                {
+                    LoginActivity.currentUser=highScore;
+                    Intent intent = new Intent(SplashScreen.this, ItemList.class);
+                    startActivity(intent);
+                }
             }
         },2500);
 
