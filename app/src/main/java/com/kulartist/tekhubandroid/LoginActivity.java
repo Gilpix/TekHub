c73adbd34754193ccfe6dc4fbce10e3cfd92605e
Gilpix <kuldeepsingh5005@gmail.com>
2020-03-12 12:06:00 -0400

    alertbox for exiting the application and user input data validation added.

--- app/src/main/java/com/kulartist/tekhubandroid/LoginActivity.java
+++ app/src/main/java/com/kulartist/tekhubandroid/LoginActivity.java
@@ -2,9 +2,17 @@ package com.kulartist.tekhubandroid;
 
 import androidx.appcompat.app.AppCompatActivity;
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.SharedPreferences;
+import android.graphics.Color;
+import android.graphics.Typeface;
+import android.graphics.drawable.ColorDrawable;
 import android.os.AsyncTask;
 import android.os.Bundle;
+import android.os.Process;
 import android.view.View;
 import android.view.inputmethod.InputMethodManager;
 import android.widget.EditText;
@@ -14,6 +22,7 @@ import com.kulartist.tekhubadmin.Dashboard;
 
 import org.json.JSONException;
 import org.json.JSONObject;
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
@@ -21,11 +30,13 @@ import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
 
+import static com.kulartist.tekhubandroid.SplashScreen.currentIP;
+
 public class LoginActivity extends AppCompatActivity {
 
     EditText usrID,password;
-    private String userEmail,userPass;
-    public static String currentUser, currentIP;
+    private String userID,userPass;
+    public static String currentUser;
 
 
     @Override
@@ -33,13 +44,51 @@ public class LoginActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
-        currentIP="192.168.1.182";
-       currentIP="192.168.0.102";
-        //currentIP="172.20.10.5";
+        //currentIP="192.168.1.182";
+       //currentIP="192.168.0.102";
+       // currentIP="172.20.10.5";
 
         usrID=findViewById(R.id.usrid);
         password=findViewById(R.id.password);
     }
+
+    @Override
+    public void onBackPressed() {
+        alertBoxDisplay();
+        //super.onBackPressed();
+    }
+
+    public void alertBoxDisplay()
+    {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setMessage("Do you really want to exit ?")
+                .setCancelable(false)
+                .setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+
+                        moveTaskToBack(true);
+                        Process.killProcess(Process.myPid());
+                        System.exit(1);
+                        LoginActivity.super.onBackPressed();
+                    }
+                })
+                .setNegativeButton("No", null);
+        //Creating dialog box
+        AlertDialog alert = builder.create();
+        //Setting the title manually
+        alert.setTitle("TekHub");
+        //alert.setCustomTitle(textView);
+        alert.getWindow().setBackgroundDrawable(new ColorDrawable(Color.DKGRAY));
+        alert.setIcon(R.drawable.tehublogo);
+        alert.show();
+        // alert.getButton(AlertDialog.BUTTON_POSITIVE).setBackgroundResource(R.color.white);
+        alert.getButton(AlertDialog.BUTTON_POSITIVE).setTypeface(null, Typeface.BOLD);
+        alert.getButton(AlertDialog.BUTTON_POSITIVE).setPadding(20,20,20,20);
+        alert.getButton(AlertDialog.BUTTON_NEGATIVE).setTypeface(Typeface.DEFAULT, Typeface.BOLD);
+        //alert.getButton(AlertDialog.BUTTON_NEGATIVE).setBackgroundResource((R.drawable.circular_button_white));
+
+    }
+
 
     public void signUp(View view) {
 
@@ -60,12 +109,25 @@ public class LoginActivity extends AppCompatActivity {
         hideKeyboardwithoutPopulate(LoginActivity.this);
         if(!usrID.getText().toString().isEmpty() && !password.getText().toString().isEmpty())
         {
-            userEmail=usrID.getText().toString();
+            userID =usrID.getText().toString();
             userPass=password.getText().toString();
-            new LoginActivity.SignInUser(userEmail, userPass).execute();
+
+            if(userID.length()==7&&userPass.length()>=5) {
+                new LoginActivity.SignInUser(userID, userPass).execute();
+            }
+            else if(userID.length()!=7){
+                usrID.setError("Enter Valid ID");
+                usrID.requestFocus();
+            }
+            else if(userPass.length()<5){
+                password.setError("Enter Valid Password");
+                password.requestFocus();            }
         }
-        else
-            Toast.makeText(LoginActivity.this,"Please Enter all fields",Toast.LENGTH_SHORT).show();
+        else {
+            usrID.requestFocus();
+            password.requestFocus();
+            Toast.makeText(LoginActivity.this, "Please Enter all fields", Toast.LENGTH_SHORT).show();
+        }
     }
 
 
@@ -139,18 +201,38 @@ public class LoginActivity extends AppCompatActivity {
 
         @Override
         protected void onPostExecute(Void result){
-            String message="";
 
             if(userStatus.equals("ok")) {
-                if(usrActive.equals("1")) {
-                    currentUser = usrId;
-                    new userIdSingleLoggedIn(usrId).execute();
+
+                if(usrId.equals("1234567")){
+                    Toast.makeText(LoginActivity.this,"Login Successful - " +userStatus,Toast.LENGTH_SHORT).show();
+
+                    Intent i = new Intent(LoginActivity.this, Dashboard.class);
+                    currentUser=usrId;
+                    startActivity(i);
                 }
-                else
-                    Toast.makeText(LoginActivity.this,"User Already Logged in other device ",Toast.LENGTH_SHORT).show();
+                else{
+                    if(usrActive.equals("1")) {
+                        SharedPreferences sp = getSharedPreferences("saveUser" , Context.MODE_PRIVATE);
+                        SharedPreferences.Editor editor = sp.edit();
+                        editor.putString("userSavedId", usrId);
+                        editor.commit();
+                        currentUser = usrId;
+                        new userIdSingleLoggedIn(usrId).execute();
+                        Toast.makeText(LoginActivity.this,"Login Successful - " +userStatus,Toast.LENGTH_SHORT).show();
+                Intent i = new Intent(LoginActivity.this, ItemList.class);
+                startActivity(i);
+                    }
+                    else
+                        Toast.makeText(LoginActivity.this,"User Already Logged in other device ",Toast.LENGTH_SHORT).show();
+
+                }
+
+//
             }
             else
                 Toast.makeText(LoginActivity.this,"Wrong Credentials - " +userStatus,Toast.LENGTH_SHORT).show();
+
             super.onPostExecute(result);
         }
     }
@@ -203,37 +285,8 @@ public class LoginActivity extends AppCompatActivity {
                 e.printStackTrace();
             }
             return null;
-
         }
 
-        @Override
-        protected void onPostExecute(Void result){
-
-            if(userStatus.equals("ok")) {
-
-                if(usrId.equals("1234")){
-                    Toast.makeText(LoginActivity.this,"Login Successful - " +userStatus,Toast.LENGTH_SHORT).show();
-
-                    Intent i = new Intent(LoginActivity.this, Dashboard.class);
-                    //currentUser=usrId;
-                    startActivity(i);
-                }
-                else{
-                    Toast.makeText(LoginActivity.this,"Login Successful - " +userStatus,Toast.LENGTH_SHORT).show();
-
-                    Intent i = new Intent(LoginActivity.this, ItemList.class);
-                    //currentUser=usrId;
-                    startActivity(i);
-                }
-
-                Toast.makeText(LoginActivity.this,"Login Successful - " +userStatus,Toast.LENGTH_SHORT).show();
-                Intent i = new Intent(LoginActivity.this, ItemList.class);
-                startActivity(i);
-            }
-            else
-                Toast.makeText(LoginActivity.this,"Wrong Credentials - " +userStatus,Toast.LENGTH_SHORT).show();
-            super.onPostExecute(result);
-        }
     }
 
 
